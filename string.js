b28b7a392f9085a04161da7cd4520aa0c20c30a7
pilgrim1807 <51112693+pilgrim1807@users.noreply.github.com>
2024-10-16 14:30:55 +0300

    Ответ на задание #1

---
+++ string.js
@@ -0,0 +1,13 @@
+const myName = 'Анастасия';
+const programmingLanguage = 'JavaScript';
+const courseCreatorName = 'Владилена Минина';
+const reasonText = 'хочу карьерного роста';
+const numberOfMonth = '1';
+
+console.log(`Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык 
+программирования ${programmingLanguage} на курсе по ${programmingLanguage} 
+у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}.
+До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяцев(а). 
+Я уверен(а), что пройду данный курс до конца!`);
+
+
